0f229a4a8cc724cefb5fc1080b3aab0d4c1236e5
priteshkumbhare <priteshkumbhare@gmail.com>
2024-09-21 17:38:20 +0530

    Add files

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,10 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
+<<<<<<< HEAD
     serviceWorkerVersion: "261205995"
+=======
+    serviceWorkerVersion: "1610037016"
+>>>>>>> 667ebf1 (Add files)
   }
 });
